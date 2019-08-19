b33dfcdce2b83d199349456575924d76f9284b8e
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-08-19 15:14:48 +0530

    add file:Question4.java

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Question4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        if (value == 0) {
+            System.out.println("Zero");
 
+        } else if (value > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
     }
-}
+    }
+

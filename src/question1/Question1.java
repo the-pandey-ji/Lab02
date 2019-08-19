51a86cfaf0570de0f944974616613ff991bc14f6
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-08-19 14:28:44 +0530

    as input for variable:(userName,String)

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine().trim();  //method chaining
         scanner.close();
 
     }

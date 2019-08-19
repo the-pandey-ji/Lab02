74aebfd0cf8197a6d6c8c29b904c97cf7a2fc6e2
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-08-19 14:53:55 +0530

    add input

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -1,7 +1,12 @@
 package question3;
 
+import java.util.Scanner;
+
 public class Question3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
 
     }
 }

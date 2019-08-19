4cdc883fba4e66339054defe253b5f788791f5e6
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-08-19 15:15:55 +0530

    add method:main add scanner class object

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -1,4 +1,10 @@
 package question5;
 
+import java.util.Scanner;
+
 public class Question5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }

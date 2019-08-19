9891cfafee6bdeefd631d5fb2b9769c85bdb6a08
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-08-19 15:30:29 +0530

    add file:Question7.java

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -1,7 +1,12 @@
 package question6;
 
+import java.util.Scanner;
+
 public class Question6 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ch1 = scanner.nextLine().trim().charAt(0);
+        char ch2 = scanner.nextLine().trim().charAt(0);
 
     }
 }

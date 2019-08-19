a0193a50b75e5d5446c3e6d57e8b52712441847d
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-19 23:13:59 +0530

    update values of digits in ascii

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -7,7 +7,14 @@ public class Question7 {
 
         Scanner scanner = new Scanner(System.in);
         char ch = scanner.next().charAt(0);
-
+        if ((int) ch >= 48 && (int) ch <= 57) {
+            System.out.println("digit");
+        } else if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+            System.out.println("alphabet");
+        } else {
+            System.out.println("symbol");
+        }
+        scanner.close();
 
     }
 }

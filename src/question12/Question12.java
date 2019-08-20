d615a20b33270ab2d18a12b22a2ea09d7f820fe4
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-20 23:45:04 +0530

    apply logic and add output statement

--- src/question12/Question12.java
+++ src/question12/Question12.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Question12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int num = scanner.nextInt(), temp = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                temp += 1;
+            }
+        }
+        if (temp == 2) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is a composite number.");
+        }
     }
 }

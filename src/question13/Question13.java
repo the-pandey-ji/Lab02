36e2e359988e5bbadae772935d4100d41333e3f2
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-20 23:58:18 +0530

    apply logic and add output statement

--- src/question13/Question13.java
+++ src/question13/Question13.java
@@ -1,7 +1,15 @@
 package question13;
 
+import java.util.Scanner;
+
 public class Question13 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt(), sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
     }
 }

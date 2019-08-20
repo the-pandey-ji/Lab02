959d98660973f7434d8a2922b3f9ed87d6c54718
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-20 23:20:41 +0530

    apply logic with for loop add output statement

--- src/question11/Question11.java
+++ src/question11/Question11.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Question11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+        for (int i = value1 + 1; i < value2; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+
 
     }
-}
+    }
+

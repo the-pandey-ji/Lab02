9aa53baeeddfeedada307a4d360f5df2ded43e2f
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-20 22:07:12 +0530

    add if and output statement

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Question9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        char ch = scanner.next().charAt(0);
+        if (ch <= 90 && ch >= 65) {
+            System.out.println((char) (ch + 32));
+        } else {
+            System.out.println((char) (ch - 32));
+        }
     }
 }

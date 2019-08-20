f806ef1907ee7e914651d2acb9190d66a314126c
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-21 00:28:15 +0530

    apply logic and add output statement

--- src/question14/Question14.java
+++ src/question14/Question14.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Question14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt(), num1 = 0;
+        int n = num;
+        while (num != 0) {
+            num1 = (num1 * 10) + num % 10;
+            num /= 10;
+        }
+        if (n == num1) {
+            System.out.println(n + " is a palindrome");
+        } else {
+            System.out.println(n + " is not a palindrome");
+        }
 
     }
-}
+    }

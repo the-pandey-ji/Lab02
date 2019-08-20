03fc593b9a72c5c053f297a084eebf3958b2a53e
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-20 21:56:09 +0530

    add object:scanner for class:Scanner

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -1,7 +1,10 @@
 package question9;
 
+import java.util.Scanner;
+
 public class Question9 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
     }
 }

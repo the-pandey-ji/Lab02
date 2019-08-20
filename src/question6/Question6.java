2da9a7a0294af34a6063be5f9dc3d187312de606
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-20 21:21:05 +0530

    add file:Question9.java

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Question6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char ch1 = scanner.nextLine().trim().charAt(0);
-        char ch2 = scanner.nextLine().trim().charAt(0);
+        char ch1 = scanner.next().trim().charAt(0);
+        char ch2 = scanner.next().trim().charAt(0);
+        System.out.println(ch2 + " " + ch1);
         scanner.close();
     }
 }
--- src/question8/Question8.java
+++ src/question8/Question8.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class Question8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String gender = scanner.next();
+        int age = scanner.nextInt();
+        if (gender == "male") {
+            if (age <= 58 && age >= 1) {
+                System.out.println("8.4%");
+            } else {
+                System.out.println("10.5%");
+            }
 
+        } else {
+            if (age <= 58 && age >= 1) {
+                System.out.println("8.2%");
+            } else {
+                System.out.println("9.2%");
+            }
+        }
     }
 }
